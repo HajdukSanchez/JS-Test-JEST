bfdb4330c3fd26143c894befc2b56f4dd6818574
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-07-08 19:29:06 -0500

    :white_check_mark: [ADD] Array test

---
+++ __test__/arrays.test.js
@@ -0,0 +1,13 @@
+import { arrayColors, arrayFruits } from '../arrays'
+
+describe('Array content a specific element', () => {
+  test('Contains banana ?', () => {
+    expect(arrayFruits()).toContain('banana')
+  })
+  test('Not contains platano ?', () => {
+    expect(arrayFruits()).not.toContain('platano')
+  })
+  test('Array length', () => {
+    expect(arrayFruits()).toHaveLength(6)
+  })
+})
---
+++ arrays.js
@@ -0,0 +1,5 @@
+const fruits = ['banana', 'melon', 'sandal', 'orange', 'lemon', 'apple']
+const colors = ['blue', 'red', 'yellow', 'gray', 'black']
+
+export const arrayFruits = () => fruits
+export const arrayColors = () => colors
