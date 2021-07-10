41d4d8a8fc7a3fbef90e98f2be5dcbe16333bfe4
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-07-10 12:09:33 -0500

    :white_check_mark: [ADD] Resolve and Reject tests

--- __test__/promise.test.js
+++ __test__/promise.test.js
@@ -1,12 +1,20 @@
 import { getDataFromApi } from '../promise'
 
 describe('Testing promises', () => {
-  test('API request', (done) => {
+  test('Should API request', (done) => {
     const URL = 'https://rickandmortyapi.com/api/character/'
     getDataFromApi(URL).then((data) => {
       expect(data.results.length).toBeGreaterThan(0)
       // We use DONE because this is a async request, so when the request is done, JEST is going to make the comparative
       done()
     })
+  })
+
+  test('Should resolved', () => {
+    return expect(Promise.resolve('Hello')).resolves.toBe('Hello')
+  })
+
+  test('Should rejected', () => {
+    return expect(Promise.reject('Error')).rejects.toBe('Error')
   })
 })
