1413a309dd8ff9da2731cacfd4862aaf612e3319
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-07-10 12:36:01 -0500

    :white_check_mark: [ADD] Async/Await application tests

--- __test__/async.test.js
+++ __test__/async.test.js
@@ -2,7 +2,7 @@ import { getDataFromApi } from '../promise'
 
 describe('Testing Async/Await', () => {
   // This is a test with two validations
-  test('Should API Request', async () => {
+  test('Should API request', async () => {
     const API = 'https://rickandmortyapi.com/api/character/'
     const API_GET_CHARACTER = 'https://rickandmortyapi.com/api/character/1'
 
@@ -12,5 +12,19 @@ describe('Testing Async/Await', () => {
     await getDataFromApi(API_GET_CHARACTER).then((data) => {
       expect(data.name).toEqual('Rick Sanchez')
     })
+  })
+
+  test('Should API request with error', async () => {
+    /* This is a URL that gave us the error that we want to receive
+    In this case we want a 404 error */
+    const API_ERROR = 'http://httpstat.us/404'
+    const petition = getDataFromApi(API_ERROR)
+    // This test mean that if the test pass, we have a problem in our development
+    await expect(petition).rejects.toEqual(Error('Request failed with status code 404'))
+  })
+
+  test('Should API request resolve and reject', async () => {
+    await expect(Promise.resolve('Hello')).resolves.toBe('Hello')
+    await expect(Promise.reject('Error')).rejects.toBe('Error')
   })
 })
