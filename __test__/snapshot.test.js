dabbdd9a0745681a31100460b34e6094834d003e
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-07-12 17:15:24 -0500

    :white_check_mark: [ADD] Snapshot extensions

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -7,4 +7,24 @@ describe('Snapshot test', () => {
     expect(getCharacter(rick)).toMatchSnapshot()
     // We need to run the test for generate a snapshot
   })
+
+  test('Should Snapshot always fail', () => {
+    // This test always has a fail because her value is not constant
+    const user = {
+      createAt: new Date(),
+      id: Math.floor(Math.random() * 20), // Random element 0-19
+    }
+    /* expect(user).toMatchSnapshot() */
+  })
+
+  test('Should Snapshot with exceptions', () => {
+    const user = {
+      id: Math.floor(Math.random() * 20), // Dynamic value
+      name: 'Jozek Hajduk', // Static value
+    }
+    expect(user).toMatchSnapshot({
+      /* We tell that we don't matter the ID Value */
+      id: expect.any(Number), // Here be generate the exception
+    })
+  })
 })
